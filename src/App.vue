8d7a9e02fdac0ca42cc70c87832b82024864d7fc
ghiottoandrea <andreaghiotto9@gmail.com>
2024-04-08 12:13:59 +0200

    add setup project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script>
+import headerApp from './components/headerApp.vue';
+import mainApp from './components/mainApp.vue';
+import footerApp from './components/footerApp.vue';
+
+export default {
+  name: App,
+  components: {
+    headerApp,
+    mainApp,
+    footerApp,
+  }
+}
 </script>
 
 <template>
 
+<headerApp />
+<mainApp />
+<footerApp />
+
 </template>
 
-<style>
-
-</style>
+<style></style>
